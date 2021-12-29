c83241298dde12a8311e97b172d2ce2c493385c3
polyaflaww <polyaflaww@gmail.com>
2021-12-29 19:52:00 +0500

    дз 8.3

---
+++ дз 8.3/дз 8.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace дз_8._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вычисление возможных значений переменной n в гармоническом ряде, при котором сумма элементов меньше значения а");
+            Console.WriteLine("Введите число а для расчета всех значений, a > 1");
+            var a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Все значения n, при которых значение выражения меньше а:");
+            double n = 1;
+            double sum = 0;
+
+            while (sum < a)
+            {
+                sum += 1 / n;
+                Console.WriteLine(n);
+                n++;
+            }
+        }
+    }
+}
