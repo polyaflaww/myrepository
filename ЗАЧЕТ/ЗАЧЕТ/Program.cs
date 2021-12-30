688c309bf5f4cb146c5376d0aa984df19725bfdc
polyaflaww <polyaflaww@gmail.com>
2021-12-30 14:28:51 +0500

    зачет

---
+++ ЗАЧЕТ/ЗАЧЕТ/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ЗАЧЕТ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работа с сиракузской последовательностью");
+            Console.WriteLine("Введите натуральное число n");
+            int n;
+            n = int.Parse(Console.ReadLine());
+            int sum = 1;
+            while (n != 1)
+            {
+                if (n % 2 == 0)
+                { 
+                    n = n / 2;
+                    sum += 1;
+                }
+                else
+                {
+                    n = 3 * n + 1;
+                    sum += 1;
+                 }
+            }
+                Console.WriteLine("Длина сиракузской последовательности равна: " + sum);
+
+            int nMaximum = 0;
+            if (n > nMaximum)
+            {
+                nMaximum = n;
+            }
+               Console.WriteLine("Максимальное число в последовательности: " + nMaximum);
+        }
+        }
+    }
+
