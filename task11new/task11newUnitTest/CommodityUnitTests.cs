b3121a8664e530554843094ad5acb1338d54ec68
polyaflaww <polyaflaww@gmail.com>
2022-06-20 11:37:51 +0500

    задание 11

---
+++ task11new/task11newUnitTest/CommodityUnitTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task11new;
+using NUnit.Framework;
+
+namespace task11newUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    [TestFixture]
+    public class CommodityUnitTests
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var chair = CreateTestCommodity();
+
+            Assert.AreEqual(123456, chair.VendorCode);
+            Assert.AreEqual("Стул деревянный", chair.Name);
+            Assert.AreEqual(20000, chair.WholesaPricePerItem);
+            Assert.AreEqual(40000, chair.RetailPricePerItem);
+            Assert.AreEqual(TypeOfUnit.Number, chair.UnitOfGoods);
+            Assert.AreEqual("Стул из красного дерева, выполненный в Италии", chair.Description);
+            Assert.AreEqual(43, chair.StockAvailability);
+        }
+        [Test]
+        public void ToString_Commodity_NameSpaceDescription()
+        {
+            var chair = CreateTestCommodity();
+
+            Assert.AreEqual("Стул деревянный Стул из красного дерева, выполненный в Италии", chair.ToString());
+        }
+        private Commodity CreateTestCommodity()
+        {
+            return new Commodity(123456, "Стул деревянный", 20000, 40000, TypeOfUnit.Number);
+        }
+    }
+}
